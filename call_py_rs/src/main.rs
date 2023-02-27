5115861546b22d8ddd2ca43990279b7ff244088f
drunisa007 <newarunkatwal@gmail.com>
2023-02-27 15:02:21 +0630

    didn't succeed with pyo3 trying pyembed

--- call_py_rs/src/main.rs
+++ call_py_rs/src/main.rs
@@ -1,14 +1,23 @@
-use std::{path::Path};
+use std::path::Path;
 
-use pyo3::{Python, types::PyModule};
 
 fn main() {
-    println!("Hello, world!");
-    let custom_path = Path::new("../../build/aarch64-unknown-linux-gnu/debug/install/linux_rsoxygen");
+    //println!("Hello, world!");
+    // let custom_path = Path::new(
+    //     "/home/aung/linux_rsoxygen/build/aarch64-unknown-linux-gnu/debug/install/linux_rsoxygen",
+    // );
 
-    println!("{:?}",custom_path);
+    const PYTHON_EXECUTABLE: &'static [u8] = include_bytes!(
+        "/home/aung/linux_rsoxygen/build/aarch64-unknown-linux-gnu/debug/install/linux_rsoxygen"
+    );
 
-   
-                                
+    let code = std::str::from_utf8(PYTHON_EXECUTABLE).unwrap();
 
+    // //println!("{:?}",custom_path);
+
+    // const PYTHON_EXECUTABLE: &'static [u8] = include_bytes!("../../build/aarch64-unknown-linux-gnu/debug/install/linux_rsoxygen");
+
+    // let module_name = "hello";
+    // //let script_file = "main.py";
+    // let code = std::str::from_utf8(PYTHON_EXECUTABLE).unwrap();
 }
